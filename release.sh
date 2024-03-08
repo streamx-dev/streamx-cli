06e5623bcd0bf2065ae5b50f1b23daf88c2c00e9
Michal Cukierman <michal.cukierman@ds.pl>
2024-03-08 14:02:05 +0100

    build: Release 0.0.1-alpha5

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-./mvnw versions:set -DnewVersion=0.0.1-alpha4
+./mvnw versions:set -DnewVersion=0.0.1-alpha5
 ./mvnw versions:commit
 ./mvnw clean install
 ./mvnw jreleaser:assemble jreleaser:full-release -Djreleaser.dry.run=false
