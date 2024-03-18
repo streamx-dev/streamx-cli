e847e7a16717ca08ea2fa1c3caccd374e8d496b1
Jakub Gardo <jakub.gardo@ds.pl>
2024-03-18 09:14:10 +0100

    [DXP-645] Log changes

---
+++ src/main/java/dev/streamx/cli/run/TestContainerLogFilter.java
@@ -0,0 +1,25 @@
+package dev.streamx.cli.run;
+
+
+import io.quarkus.logging.LoggingFilter;
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.stream.Stream;
+
+@LoggingFilter(name = "tc-pull-filter")
+public class TestContainerLogFilter  implements Filter {
+
+  @Override
+  public boolean isLoggable(LogRecord record) {
+    Level level = record.getLevel();
+
+    return level.intValue() >= Level.WARNING.intValue() || Stream.of(
+        "Pulling image",
+            "Pull complete.",
+            "Pulling docker image",
+            "Starting to pull image"
+        )
+        .anyMatch(start -> record.getMessage().startsWith(start));
+  }
+}
