a8d8ac176f51dd93d4907b3429565e7284520213
Jakub Gardo <jakub.gardo@ds.pl>
2024-04-24 09:13:56 +0200

    [DXP-791] Release version 0.2.4

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-./mvnw versions:set -DnewVersion=0.2.3
+./mvnw versions:set -DnewVersion=0.2.4
 ./mvnw versions:commit
 ./mvnw clean install
 ./mvnw jreleaser:assemble jreleaser:full-release -Djreleaser.dry.run=false
