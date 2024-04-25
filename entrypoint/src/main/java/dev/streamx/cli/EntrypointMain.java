d09cb7514048bb1b89cfedd5876a0deea465a387
Jakub Gardo <155527092+jgardo-ds@users.noreply.github.com>
2024-04-25 12:31:48 +0200

    [DXP-780] Introduction multimodule structure

---
+++ entrypoint/src/main/java/dev/streamx/cli/EntrypointMain.java
@@ -0,0 +1,9 @@
+package dev.streamx.cli;
+
+public class EntrypointMain {
+
+  public static void main(String[] args) {
+    // TODO check version https://teamds.atlassian.net/browse/DXP-790
+    System.out.println("Hello world!");
+  }
+}
