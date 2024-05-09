b0e582b2f145ca230cb3a0eaa86e71c4669f11f5
Jakub Gardo <jakub.gardo@ds.pl>
2024-05-09 19:25:09 +0200

    [DXP-801] Override prod log file name on startup basing on startup time

--- entrypoint/src/main/java/dev/streamx/cli/EntrypointMain.java
+++ entrypoint/src/main/java/dev/streamx/cli/EntrypointMain.java
@@ -1,23 +1,26 @@
 package dev.streamx.cli;
 
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EntrypointMain {
+
+  private static final SimpleDateFormat DATE_FORMAT =
+      new SimpleDateFormat("yyyy_MM_dd__HH_mm_ss_SSS");
+  private static final String LOG_FILE_PATH_PROPERTY_NAME = "%prod.quarkus.log.file.path";
+  private static final String STREAMX_LOG_FILE_NAME_PATTERN = ".streamx/streamx-%s.log";
 
   public static void main(String[] args) {
     int javaVersion = getJavaVersion();
 
-    if (javaVersion >= 17) {
-      try {
-        Class<?> streamxCommand = Class.forName("dev.streamx.cli.StreamxCommand");
-        Method main = streamxCommand.getDeclaredMethod("main", String[].class);
-        main.invoke(null, new Object[]{args});
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    } else {
+    if (javaVersion < 17) {
       System.out.println("Java 17 or higher is required!");
+      return;
     }
+
+    overrideLogFileName();
+    runStreamxCommand(args);
   }
 
   private static int getJavaVersion() {
@@ -31,5 +34,23 @@ public class EntrypointMain {
       }
     }
     return Integer.parseInt(version);
+  }
+
+  private static void overrideLogFileName() {
+    String date = DATE_FORMAT.format(new Date());
+    String streamxLogPath = String.format(STREAMX_LOG_FILE_NAME_PATTERN, date);
+
+    System.setProperty(LOG_FILE_PATH_PROPERTY_NAME, streamxLogPath);
+  }
+
+  private static void runStreamxCommand(String[] args) {
+    try {
+      Class<?> streamxCommand = Class.forName("dev.streamx.cli.StreamxCommand");
+      Method main = streamxCommand.getDeclaredMethod("main", String[].class);
+
+      main.invoke(null, new Object[]{args});
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- entrypoint/src/test/java/dev/streamx/cli/EntrypointMainTest.java
+++ entrypoint/src/test/java/dev/streamx/cli/EntrypointMainTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class EntrypointMainTest {
 
+  private static final String STREAMX_LOG_REGEX =
+      "\\.streamx/streamx-[0-9]{4}_[0-9]{2}_[0-9]{2}__[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{3}\\.log";
+
   @BeforeEach
   void setup() {
     StreamxCommand.clearLaunched();
@@ -46,5 +49,15 @@ public class EntrypointMainTest {
     // then
     Assertions.assertFalse(StreamxCommand.isLaunched());
     Assertions.assertEquals("Java 17 or higher is required!\n", byteArrayOutputStream.toString());
+  }
+
+  @Test
+  void shouldOverrideProdFileLogName() {
+    // when
+    EntrypointMain.main(new String[] {});
+
+    // then
+    String fileName = System.getProperty("%prod.quarkus.log.file.path");
+    Assertions.assertTrue(fileName.matches(STREAMX_LOG_REGEX));
   }
 }
