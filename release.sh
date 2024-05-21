4aa46e9895fcfde302a16179a0c514a88f3661eb
Jakub Gardo <jakub.gardo@ds.pl>
2024-05-21 09:15:52 +0200

    [TECH] Release 0.3.0

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
-./mvnw versions:set -DnewVersion=0.2.5
+./mvnw versions:set -DnewVersion=0.3.0
 ./mvnw versions:commit
 ./mvnw clean install
-./mvnw jreleaser:assemble jreleaser:full-release -Djreleaser.dry.run=false
+cd distribution
+../mvnw jreleaser:assemble jreleaser:full-release -Djreleaser.dry.run=false
