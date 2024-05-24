00ac826d178e77c424dd00d39f6f2cf9b7cb67b2
andrzejmikolajczakds <andrzej.mikolajczak@ds.pl>
2024-05-24 09:27:21 +0200

    DXP-865 make possible to disable ssl validation for CLI

--- core/src/main/java/dev/streamx/cli/ingestion/HttpClientProvider.java
+++ core/src/main/java/dev/streamx/cli/ingestion/HttpClientProvider.java
@@ -2,14 +2,35 @@ package dev.streamx.cli.ingestion;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Dependent;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Dependent
 public class HttpClientProvider {
 
+  @ConfigProperty(name = "validation.ssl.enabled", defaultValue = "true")
+  boolean sslEnabled;
+
   @ApplicationScoped
-  CloseableHttpClient ingestionHttpClient() {
-    return HttpClients.createDefault();
+  CloseableHttpClient ingestionHttpClient()
+      throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+
+    HttpClientBuilder builder = HttpClients
+        .custom();
+    if (!sslEnabled) {
+      builder
+          .setSSLContext(
+              new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build())
+          .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+    }
+    return builder.build();
   }
 }
