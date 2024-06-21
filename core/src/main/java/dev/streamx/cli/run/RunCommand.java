f567e7352d32deb25587ad6e62ee575e108b7a13
Jakub Gardo <155527092+jgardo-ds@users.noreply.github.com>
2024-06-21 15:33:43 +0200

    [DXP-842] Validate docker environment

--- core/src/main/java/dev/streamx/cli/run/RunCommand.java
+++ core/src/main/java/dev/streamx/cli/run/RunCommand.java
@@ -2,9 +2,12 @@ package dev.streamx.cli.run;
 
 import static dev.streamx.runner.main.Main.StreamxApp.printSummary;
 
+import dev.streamx.cli.exception.DockerException;
 import dev.streamx.cli.run.MeshDefinitionResolver.MeshDefinition;
 import dev.streamx.runner.StreamxRunner;
 import dev.streamx.runner.event.ContainerStarted;
+import dev.streamx.runner.validation.excpetion.DockerContainerNonUniqueException;
+import dev.streamx.runner.validation.excpetion.DockerEnvironmentException;
 import io.quarkus.runtime.Quarkus;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
@@ -45,15 +48,24 @@ public class RunCommand implements Runnable {
     try {
       print(BANNER);
       MeshDefinition result = meshDefinitionResolver.resolve(meshSource);
+      String meshPath = result.path().normalize().toAbsolutePath().toString();
 
       print("Setting up system containers...");
 
-      this.runner.startBase(result.serviceMesh());
+      try {
+        this.runner.initialize(result.serviceMesh(), meshPath);
+      } catch (DockerContainerNonUniqueException e) {
+        throw DockerException.nonUniqueContainersException(e.getContainers());
+      } catch (DockerEnvironmentException e) {
+        throw DockerException.dockerEnvironmentException();
+      }
+
+      this.runner.startBase();
 
       print("");
       print("Starting DX Mesh...");
 
-      this.runner.startMesh(result.serviceMesh());
+      this.runner.startMesh();
 
       printSummary(this.runner, result.path());
       Quarkus.waitForExit();
