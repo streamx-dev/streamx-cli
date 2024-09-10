6468e94be833ff2a501b8285f6c03ed7d3c703da
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2024-09-10 13:34:21 +0200

    [DXP-994] Shorten command line errors when user passes invalid params (#69)

---
+++ core/src/main/java/dev/streamx/cli/ParameterExceptionHandler.java
@@ -0,0 +1,38 @@
+package dev.streamx.cli;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.io.PrintWriter;
+import org.jboss.logging.Logger;
+import picocli.CommandLine;
+import picocli.CommandLine.IParameterExceptionHandler;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.UnmatchedArgumentException;
+
+@ApplicationScoped
+public class ParameterExceptionHandler implements IParameterExceptionHandler {
+
+  @Inject
+  Logger log;
+
+  @Override
+  public int handleParseException(ParameterException ex, String[] args) {
+    log.error("Parameter exception occurred.", ex);
+
+    CommandLine cmd = ex.getCommandLine();
+    PrintWriter writer = cmd.getErr();
+
+    writer.println(ex.getMessage());
+    UnmatchedArgumentException.printSuggestions(ex, writer);
+    writer.println(cmd.getHelp().fullSynopsis());
+
+    CommandSpec spec = cmd.getCommandSpec();
+    writer.printf("Try '%s --help' for more information.%n", spec.qualifiedName());
+
+    if (cmd.getExitCodeExceptionMapper() != null) {
+      return cmd.getExitCodeExceptionMapper().getExitCode(ex);
+    }
+    return spec.exitCodeOnInvalidInput();
+  }
+}
--- core/src/test/java/dev/streamx/cli/ingestion/IngestionArgumentsValidationTest.java
+++ core/src/test/java/dev/streamx/cli/ingestion/IngestionArgumentsValidationTest.java
@@ -37,14 +37,8 @@ public class IngestionArgumentsValidationTest extends BaseIngestionCommandTest {
         Error: Missing required argument(s): (<channel> <key>)
         Usage: streamx unpublish [-hV] [[--ingestion-url=<propagateIngestionUrl>]]
                                  (<channel> <key>)
-        Send unpublication trigger
-              <channel>   Channel that message will be published to
-              <key>       Message key
-          -h, --help      Show this help message and exit.
-              --ingestion-url=<propagateIngestionUrl>
-                          Address of 'rest-ingestion-service'
-                            Default: http://localhost:8080
-          -V, --version   Print version information and exit.""");
+
+        Try 'streamx unpublish --help' for more information.""");
   }
 
   @Test
@@ -58,50 +52,8 @@ public class IngestionArgumentsValidationTest extends BaseIngestionCommandTest {
         Usage: streamx publish [-hV] [[--ingestion-url=<propagateIngestionUrl>]]
                                (<channel> <key> [payloadFile]) [[[-s=<string> |
                                -b=<binary> | -j=<json>]]...]
-        Send publication data
-              <channel>       Channel that message will be published to
-              <key>           Message key
-              [payloadFile]   File containing the payload to be published.
-                              This is an optional parameter.
-                              This parameter is equivalent to
-                              -j=file://<payloadFile> specified before other Payload
-                                Defining Option.
-                
-                              If this parameter is present, it has the highest priority
-                                in defining the payload.
-          -h, --help          Show this help message and exit.
-              --ingestion-url=<propagateIngestionUrl>
-                              Address of 'rest-ingestion-service'
-                                Default: http://localhost:8080
-          -V, --version       Print version information and exit.
-                
-        Payload Defining Options:
-            Payload can be defined by specifying an explicit full payload.
-            For example, publish -j "{}" (...)
-            will send {} to the ingestion service.
-                
-            A payload can also be created by specifying one or more
-            JSONPath expressions with payload fragments.
-            For example, publish -s type=string -s content.bytes=hello (...)
-            will send {"type":"string","content":{"bytes":"hello"}}
-            to the ingestion service.
-                
-            There are few ways to define the parameter value:
-            * if value has prefix file:// then value will be loaded
-              from file with given (relative or absolute) path
-            * otherwise raw value will be used as value
-                
-            Payload fragment type:
-            * if the value is a string fragment, use -s option
-            * if the value is a JSON fragment, use -j option
-            * if the value is a binary value fragment use -b option
-                
-          -b, --binary-fragment=<binary>
-                              Defines a payload fragment of binary type
-          -j, --json-fragment=<json>
-                              Defines a payload fragment of JSON node type
-          -s, --string-fragment=<string>
-                              Defines a payload fragment of string type""");
+
+        Try 'streamx publish --help' for more information.""");
   }
 
   @Test
@@ -114,14 +66,8 @@ public class IngestionArgumentsValidationTest extends BaseIngestionCommandTest {
         Error: Missing required argument(s): <key>
         Usage: streamx unpublish [-hV] [[--ingestion-url=<propagateIngestionUrl>]]
                                  (<channel> <key>)
-        Send unpublication trigger
-              <channel>   Channel that message will be published to
-              <key>       Message key
-          -h, --help      Show this help message and exit.
-              --ingestion-url=<propagateIngestionUrl>
-                          Address of 'rest-ingestion-service'
-                            Default: http://localhost:8080
-          -V, --version   Print version information and exit.""");
+
+        Try 'streamx unpublish --help' for more information.""");
   }
 
   @Test
