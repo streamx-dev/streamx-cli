f2dfcfe7b9b27b5b8cc23a9949dbe8a8112dea09
Michal Cukierman <michal.cukierman@ds.pl>
2025-02-19 19:13:30 +0100

    DXP-1673 Fixed undeploy mesh test

--- core/src/test/java/dev/streamx/cli/command/cloud/undeploy/UndeployCommandIT.java
+++ core/src/test/java/dev/streamx/cli/command/cloud/undeploy/UndeployCommandIT.java
@@ -3,10 +3,12 @@ package dev.streamx.cli.command.cloud.undeploy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.streamx.cli.command.cloud.KubernetesClientProfile;
+import dev.streamx.cli.command.cloud.ProjectUtils;
 import io.quarkus.test.junit.TestProfile;
 import io.quarkus.test.junit.main.LaunchResult;
 import io.quarkus.test.junit.main.QuarkusMainLauncher;
 import io.quarkus.test.junit.main.QuarkusMainTest;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 @QuarkusMainTest
@@ -15,7 +17,8 @@ class UndeployCommandIT {
 
   @Test
   void shouldUndeployProject(QuarkusMainLauncher launcher) {
-    LaunchResult result = launcher.launch("undeploy");
+    String meshPath = ProjectUtils.getResourcePath(Path.of("with-configs.yaml")).toString();
+    LaunchResult result = launcher.launch("undeploy", "-f=" + meshPath);
     assertThat(result.getOutput()).contains(
         "StreamX project successfully undeployed from 'default' namespace.");
   }
