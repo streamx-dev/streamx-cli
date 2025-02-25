1a9f48dd703393c2d20e64f5e778e11b114a11a1
Michał Cukierman <michal.cukierman@ds.pl>
2025-02-25 12:54:32 +0100

    DXP-1676 Support for properties in batch publishing (#174)

--- core/src/main/java/dev/streamx/cli/command/ingestion/IngestionMessageJsonFactory.java
+++ core/src/main/java/dev/streamx/cli/command/ingestion/IngestionMessageJsonFactory.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.Map;
 
 @ApplicationScoped
 public class IngestionMessageJsonFactory {
@@ -12,10 +13,10 @@ public class IngestionMessageJsonFactory {
   private static final ObjectMapper mapper = new ObjectMapper();
 
   /**
-   * @param key of resource
-   * @param action publish or unpublish
+   * @param key            of resource
+   * @param action         publish or unpublish
    * @param payloadContent to include as a payload in returned JsonNode
-   * @param payloadType type matching registered ingestion API schema
+   * @param payloadType    type matching registered ingestion API schema
    * @return JsonNode representation of {@link dev.streamx.clients.ingestion.publisher.Message}
    */
 
@@ -25,12 +26,36 @@ public class IngestionMessageJsonFactory {
       JsonNode payloadContent,
       String payloadType
   ) {
+    return from(key, action, payloadContent, Map.of(), payloadType);
+  }
+
+  /**
+   * @param key            of resource
+   * @param action         publish or unpublish
+   * @param payloadContent to include as a payload in returned JsonNode
+   * @param properties     to use as payload properties
+   * @param payloadType    type matching registered ingestion API schema
+   * @return JsonNode representation of {@link dev.streamx.clients.ingestion.publisher.Message}
+   */
+
+  public JsonNode from(
+      String key,
+      String action,
+      JsonNode payloadContent,
+      Map<String, String> properties,
+      String payloadType
+  ) {
     ObjectNode root = mapper.createObjectNode();
+    ObjectNode propertiesObject = mapper.createObjectNode();
+
+    if (properties != null) {
+      properties.forEach(propertiesObject::put);
+    }
 
     root.put("key", key);
     root.put("action", action);
     root.putNull("eventTime");
-    root.putObject("properties");
+    root.set("properties", propertiesObject);
     ObjectNode payload = root.putObject("payload");
     payload.set(payloadType, payloadContent);
     return root;
