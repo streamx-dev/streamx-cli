233e87057f6641eecb52f71da6fc020cd95b6ad2
Jakub Gardo <155527092+jgardo-ds@users.noreply.github.com>
2025-04-23 15:43:52 +0200

    [DXP-1821] Fix `streamx init` on Windows

--- core/src/main/java/dev/streamx/cli/util/os/CmdCommandStrategy.java
+++ core/src/main/java/dev/streamx/cli/util/os/CmdCommandStrategy.java
@@ -5,7 +5,7 @@ public class CmdCommandStrategy implements OsCommandStrategy {
 
   @Override
   public ProcessBuilder create(String command) {
-    String[] commandArray = {"cmd.exe", "-c", command};
+    String[] commandArray = {"cmd.exe", "/c", command};
     return new ProcessBuilder(commandArray);
   }
 }
