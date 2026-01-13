acf90a6a668b74aff174aa8e9bf275c9f1dfd7e9
Kiryl Valkovich <kiryl.valkovich@ds.pl>
2026-01-13 16:38:20 +0300

    Revert EntrypointMain.java

--- entrypoint/src/main/java/dev/streamx/cli/EntrypointMain.java
+++ entrypoint/src/main/java/dev/streamx/cli/EntrypointMain.java
@@ -7,13 +7,33 @@ import java.util.Date;
 public class EntrypointMain {
 
   private static final SimpleDateFormat DATE_FORMAT =
-      new SimpleDateFormat("yyyy_MM_dd__HH_mm_ss_SSS");
+          new SimpleDateFormat("yyyy_MM_dd__HH_mm_ss_SSS");
   private static final String LOG_FILE_PATH_PROPERTY_NAME = "%prod.quarkus.log.file.path";
   private static final String STREAMX_LOG_FILE_NAME_PATTERN = "%s/.streamx/logs/streamx-%s.log";
 
   public static void main(String[] args) {
+    int javaVersion = getJavaVersion();
+
+    if (javaVersion < 17) {
+      System.out.println("Java 17 or higher is required!");
+      return;
+    }
+
     overrideLogFileName();
     runStreamxCommand(args);
+  }
+
+  private static int getJavaVersion() {
+    String version = System.getProperty("java.version");
+    if (version.startsWith("1.")) {
+      version = version.substring(2, 3);
+    } else {
+      int dot = version.indexOf(".");
+      if (dot != -1) {
+        version = version.substring(0, dot);
+      }
+    }
+    return Integer.parseInt(version);
   }
 
   private static void overrideLogFileName() {
